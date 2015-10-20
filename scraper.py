79a466092cfb7f00341d8859ed9dc3fcdc758113
Andrew Yang <ayang14@illinois.edu>
2015-10-20 16:40:42 -0500

    Add more methods to scraper

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
 from instagram.client import InstagramAPI
-from alchemyapi import AlchemyAPI
+#from alchemyapi import AlchemyAPI
 from datetime import datetime, timedelta
 
 api = InstagramAPI(client_id='64de641b01d648779939696d77ccff38', client_secret='043233db9a2c4c13a40a442c7bee0c43')
-alchemyapi = AlchemyAPI()
+#alchemyapi = AlchemyAPI()
 
 def fetchLatestMedia(tag_name, max_age=7):
     # tag_name - the hashtag name
@@ -26,6 +26,7 @@ def fetchMediaInfo(media):
     #   thumbnail URL
     #   high-res URL
     #   caption text
+    #   num likes
     #   comments
     #   user
     #   created time
@@ -34,9 +35,51 @@ def fetchMediaInfo(media):
         'thumbnail': media.get_thumbnail_url(),
         'link': media.get_standard_resolution_url(),
         'caption': media.caption.text,
+        'like_count': media.like_count,
         'comments': media.comments,
         'user': media.user,
         'created_time': media.created_time
+    }
+
+def countUserPosts(user):
+    # user - user object
+    # returns the number of posts the user has made
+    media, next_ = api.user_recent_media(user_id=user.id, count=100)
+    count = len(media)
+    while next_:
+        more_media, next_ = api.user_recent_media(user_id=user.id, count=100, with_next_url=next_)
+        count += len(more_media)
+    return count
+
+def countUserFollowers(user):
+    # user - user object
+    # returns the number of followers a user has
+    followers, next_ = api.user_followed_by(user_id=user.id, count=100)
+    count = len(followers)
+    while next_:
+        more_followers, next_ = api.user_followed_by(user_id=user.id, count=100, with_next_url=next_)
+        count += len(more_followers)
+    return count
+
+def countUserFollowing(user):
+    # user -  user object
+    # returns the number of people the user is following
+    following, next_ = api.user_follows(user_id=user.id, count=100)
+    count = len(following)
+    while next_:
+        more_following, next_ = api.user_follows(user_id=user.id, count=100, with_next_url=next_)
+        count += len(more_following)
+    return count
+
+def fetchUserInfo(user):
+    # fetch these info from each user:
+    #   num posts
+    #   num followers
+    #   num following
+    return {
+        'posts': countUserPosts(user),
+        'followers': countUserFollowers(user),
+        'following': countUserFollowing(user)
     }
 
 def analyzeSentiment(media):
