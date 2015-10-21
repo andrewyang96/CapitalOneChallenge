f3a582df4f7dc26ea2a15c1da58052301ea1ccb6
Andrew Yang <ayang14@illinois.edu>
2015-10-21 13:07:03 -0500

    Finish scraper script, also tweaked score influence formula

--- scraper.py
+++ scraper.py
@@ -2,9 +2,11 @@ from instagram.client import InstagramAPI
 from alchemyapi import AlchemyAPI
 from datetime import datetime, timedelta
 import math
+import json
 
 api = InstagramAPI(client_id='64de641b01d648779939696d77ccff38', client_secret='043233db9a2c4c13a40a442c7bee0c43')
 alchemyapi = AlchemyAPI()
+OUTFILE = "results.json"
 
 def fetchLatestMedia(tag_name, max_age=7):
     # tag_name - the hashtag name
@@ -85,7 +87,10 @@ def getUserInfluence(user):
     # returns user influence score based on this formula:
     # sqrt(user_posts) * (user_followers / user_following) * math.log(user_followers+1, 2)
     userInfo = fetchUserInfo(user)
-    return math.sqrt(userInfo['posts']) * (userInfo['followers']/float(userInfo['following'])) * math.log(userInfo['followers']+1, 2)
+    print user.username, "has", userInfo['posts'], "posts, has", userInfo['followers'], "followers, and is following", userInfo['following']
+    userInfluence = (math.log(userInfo['posts'], 20)+1) * (userInfo['followers']/float(userInfo['following'])) * math.log(userInfo['followers']+1, 10)
+    print "User influence is", round(userInfluence, 2)
+    return userInfluence
 
 def analyzeSentiment(media):
     # uses AlchemyAPI's sentiment analysis to parse sentiment into a raw score
@@ -93,28 +98,82 @@ def analyzeSentiment(media):
     sentiment = alchemyapi.sentiment('text', media['caption'])
     if sentiment['status'] != 'OK':
         raise Exception(sentiment['statusInfo'])
-    try:
+    if 'score' in sentiment['docSentiment']:
+        print "The raw sentiment score is", sentiment['docSentiment']['score']
         return (float(sentiment['docSentiment']['score']), sentiment['docSentiment']['type'])
-    except KeyError:
+    else:
+        print "The raw sentiment score doesn't exist: assuming 0"
         return (0.0, 'neutral')
 
 def calculateScore(media):
     user = media['user']
-    influence = getUserInfluence(user)
+    likeCount = media['like_count']
+    likeInfluence = math.sqrt(likeCount+1)
+    print "This post has", likeCount, "likes"
     try:
-        sentiment, type_ = analyzeSentiment(media)
+        influence = getUserInfluence(user)
     except Exception, e:
-        print "Error getting sentiment:", e
-        sentiment, type_ = 0.0, 'neutral'
+        print "Error getting info for user", media['user'].username
+        influence = 0.0
+        sentiment, type_ = 0.0, 'error'
+    else:
+        try:
+            sentiment, type_ = analyzeSentiment(media)
+        except Exception, e:
+            print "Error getting sentiment:", e
+            sentiment, type_ = 0.0, 'error'
     return {
-        'score': influence * sentiment,
+        'score': round(influence * sentiment * likeInfluence, 1),
         'type': type_
     }
 
-capitalonemedia = fetchLatestMedia("CapitalOne", 7)
+def serializeUser(user):
+    return {
+        'id': user.id,
+        'username': user.username
+    }
+
+print "Fetching latest posts about #CapitalOne"
+capitalonemedia = fetchLatestMedia("CapitalOne", 2)
+print "Processing latest posts about #CapitalOne"
 capitalone = map(fetchMediaInfo, capitalonemedia)
+
 print "Calculating scores"
+output = { 'posts': [] }
+sentimentCounts = {
+    'negative': 0,
+    'neutral': 0,
+    'positive': 0,
+    'error': 0
+}
+totalScore = 0
+
 for media in capitalone:
     print media['caption']
-    print calculateScore(media)
+    # process sentiment details
+    sentimentDetails = calculateScore(media)
+    sentimentScore = sentimentDetails['score']
+    sentimentType = sentimentDetails['type']
+    totalScore += sentimentScore
+    sentimentCounts[sentimentType] += 1
+    # append to output results object
+    media['sentiment_score'] = sentimentScore
+    media['sentiment_type'] = sentimentType
+    media['user'] = serializeUser(media['user'])
+    media['created_time'] = media['created_time'].__str__()
+    output['posts'].append(media)
+    print "The post's sentiment is", sentimentType, "with a score", sentimentScore
     print
+
+output['total_score'] = totalScore
+print "Total Score:", totalScore
+output['sentiment_counts'] = sentimentCounts
+print "Sentiment counts:", sentimentCounts
+output['time'] = datetime.now().__str__()
+print "Finished script at", output['time']
+
+with open(OUTFILE, "w") as outf:
+    print "Dumping output to", OUTFILE
+    json.dump(output, outf)
+
+print "Done!"
