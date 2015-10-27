e31f5cee62538ac73e065fe58f119a7084023f54
Andrew Yang <andrewyang96@gmail.com>
2015-10-27 15:59:57 -0500

    SQlite3 database

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, g
+import sqlite3
+
 app = Flask(__name__)
+DATABASE = '/scraper/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
-def index():
-    return 'Hello World!'
-
-@app.route('/template')
-def template(name=None):
+def index(name=None):
     url_for('static', filename='static/style.css')
     url_for('static', filename='static/script.js')
     return render_template('index.html', name=name)
+
+# Begin API methods
+
+# TODO
+
+# End API methods
 
 if __name__ == "__main__":
     app.run()
