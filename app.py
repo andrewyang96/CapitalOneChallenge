b9148bb5e2b1fb3d085ccb717a18a85ef1900009
Andrew Yang <andrewyang96@gmail.com>
2015-10-27 15:54:15 -0500

    Reorganize Flask app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/template')
+def template(name=None):
+    url_for('static', filename='static/style.css')
+    url_for('static', filename='static/script.js')
+    return render_template('index.html', name=name)
+
 if __name__ == "__main__":
     app.run()
-
